f180b3cea55e532a81ff7226dbec1a4deac80d7a
ShreyasJenner <shreyasjenner4@gmail.com>
2026-08-18 21:04:58 +0530

    mindwtr, bug fix in script

--- scripts/copy_docker-compose.sh
+++ scripts/copy_docker-compose.sh
@@ -3,7 +3,10 @@
 SRC_DIR="$HOME/docker/"
 DEST_DIR="$HOME/repos/homelab-configs/"
 FILENAME="docker-compose.yml"
+FILENAME2="service-list.txt"
 
 cd "$SRC_DIR" || exit 1
 
 find . -type f -name "$FILENAME" -exec cp --parents {} "$DEST_DIR" \;
+
+cp -- "${SRC_DIR%/}/${FILENAME2}" "$DEST_DIR"
